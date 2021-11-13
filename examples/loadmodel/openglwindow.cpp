0ea5a708888de78c86ca591843a79aa846bcea6a
Héber Camacho <heber.camachod@gmail.com>
2021-11-13 14:15:03 -0300

    openglwindow.cpp

---
+++ examples/loadmodel/openglwindow.cpp
@@ -0,0 +1,21 @@
+#include "openglwindow.hpp"
+
+#include <fmt/core.h>
+#include <imgui.h>
+#include <tiny_obj_loader.h>
+
+#include <cppitertools/itertools.hpp>
+#include <glm/gtx/fast_trigonometry.hpp>
+#include <glm/gtx/hash.hpp>
+#include <unordered_map>
+
+// Explicit specialization of std::hash for Vertex
+namespace std {
+template <>
+struct hash<Vertex> {
+  size_t operator()(Vertex const& vertex) const noexcept {
+    const std::size_t h1{std::hash<glm::vec3>()(vertex.position)};
+    return h1;
+  }
+};
+}  // namespace std
