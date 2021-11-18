1b7cda1cfd2d1bb7f20074c79b9e735f60633428
Héber Camacho <heber.camachod@gmail.com>
2021-11-17 23:23:22 -0300

    Separação da classe Dice para gerar vários dados

--- examples/dice/openglwindow.cpp
+++ examples/dice/openglwindow.cpp
@@ -23,7 +23,7 @@ struct hash<Vertex> {
 }  // namespace std
 
 void OpenGLWindow::initializeGL() {
-  abcg::glClearColor(0, 0, 0, 1);
+  abcg::glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
   // Enable depth buffering
   abcg::glEnable(GL_DEPTH_TEST); //descartar fragmentos dependendo da profundidade
@@ -36,59 +36,17 @@ void OpenGLWindow::initializeGL() {
   loadModelFromFile(getAssetsPath() + "dice.obj"); //carregamento do .obj
   standardize();
 
+  #if !defined(__EMSCRIPTEN__)
+    abcg::glEnable(GL_PROGRAM_POINT_SIZE);
+  #endif
+
   m_verticesToDraw = m_indices.size();
 
-  // Generate VBO
-  abcg::glGenBuffers(1, &m_VBO);
-  abcg::glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-  abcg::glBufferData(GL_ARRAY_BUFFER, sizeof(m_vertices[0]) * m_vertices.size(),
-                     m_vertices.data(), GL_STATIC_DRAW);
-  abcg::glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-  // Generate EBO
-  abcg::glGenBuffers(1, &m_EBO);
-  abcg::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
-  abcg::glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-                     sizeof(m_indices[0]) * m_indices.size(), m_indices.data(),
-                     GL_STATIC_DRAW);
-  abcg::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-  // Create VAO
-  abcg::glGenVertexArrays(1, &m_VAO);
-
-  // Bind vertex attributes to current VAO
-  abcg::glBindVertexArray(m_VAO);
-  abcg::glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-
-  // Bind vertex attributes
-  GLint positionAttribute{abcg::glGetAttribLocation(m_program, "inPosition")}; //layout(location = _)
-  if (positionAttribute >= 0) {
-    abcg::glEnableVertexAttribArray(positionAttribute);
-    abcg::glVertexAttribPointer(positionAttribute, 3, GL_FLOAT, GL_FALSE,
-                                sizeof(Vertex), nullptr);
-  }
-
-  //aqui a gente passa a cor do vértice já pronta para o shader
-  const GLint colorAttribute{abcg::glGetAttribLocation(m_program, "inColor")};
-  if (colorAttribute >= 0) {
-    abcg::glEnableVertexAttribArray(colorAttribute);
-    GLsizei offset{sizeof(glm::vec3)};
-    abcg::glVertexAttribPointer(colorAttribute, 3, GL_FLOAT, GL_FALSE,
-                                sizeof(Vertex),
-                                reinterpret_cast<void*>(offset));
-  }
-
-  abcg::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
-
-  // End of binding to current VAO
-  abcg::glBindBuffer(GL_ARRAY_BUFFER, 0);
-  abcg::glBindVertexArray(0);
-
-  inicializarDado();
+  m_dices.initializeGL(m_program, 2, m_vertices, m_indices, m_verticesToDraw);
 }
 
 //carregar e ler o arquivo .obj, armazenar vertices e indices em m_vertices e m_indices.
-void OpenGLWindow::loadModelFromFile(std::string path) {
+void OpenGLWindow::loadModelFromFile(std::string_view path) {
   tinyobj::ObjReader reader;
 
   if (!reader.ParseFromFile(path.data())) {
@@ -175,103 +133,13 @@ void OpenGLWindow::standardize() {
 }
 
 void OpenGLWindow::paintGL() {
-  //quantos segundos se passaram desde a ultima atualização da tela
-  const float deltaTime{static_cast<float>(getDeltaTime())};
-
-  //Dado sendo girado, temos que definir algumas variáveis para ilustrar seu giro de forma realista
-  if(dadoGirando){
-    
-    quadros++;
-    if(translation.x >= 1.5f) {
-      movimentoDado.x = false;
-      velocidadeAngularAleatoria();
-      velocidadeDirecionalAleatoria();
-    }
-    else if (translation.x <= -1.5f) {
-      movimentoDado.x = true;
-      velocidadeAngularAleatoria();
-      velocidadeDirecionalAleatoria();
-    }
-
-    if(translation.y >= 1.5f) {
-      movimentoDado.y = false;
-      velocidadeAngularAleatoria();
-      velocidadeDirecionalAleatoria();
-    }
-    else if (translation.y <= -1.5f) {
-      movimentoDado.y = true;
-      velocidadeAngularAleatoria();
-      velocidadeDirecionalAleatoria();
-    }
-    
-    //ir pra direita
-    if(movimentoDado.x) {
-      translation.x += velocidadeDirecional.x; 
-    }
-    //ir pra esquerda
-    else{
-      translation.x -= velocidadeDirecional.x;
-    }
-    //ir pra cima
-    if(movimentoDado.y) {
-      translation.y += velocidadeDirecional.y; 
-    }
-    //ir pra baixo
-    else{
-      translation.y -= velocidadeDirecional.y;
-    }
-
-    //fmt::print("q: {} translation: {} {}\n",quadros, translation.x, translation.y);
-    
-    //podemos finalizar o giro do dado e parar num número aleatório
-    if(quadros > maxQuadros){
-      pousarDado();
-    }
-  }
-
   
-  // angulo (em radianos) é incrementado se houver alguma rotação ativa
-  if(m_rotation.x || m_rotation.y ||m_rotation.z){
-    //ajuste de velocidade de rotação, necessário para conseguirmos pausar
-    myTime = deltaTime;
-    
-    //incrementa ângulo de {x,y,z} se rotação em torno do eixo {x,y,z} estiver ativa
-    if(m_rotation.x)
-      m_angle.x = glm::wrapAngle(m_angle.x + velocidadeAngular.x * myTime);
+    // Clear color buffer and depth buffer
+    abcg::glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    if(m_rotation.y)
-      m_angle.y = glm::wrapAngle(m_angle.y + velocidadeAngular.y * myTime);
+    abcg::glViewport(0, 0, m_viewportWidth, m_viewportHeight);
 
-    if(m_rotation.z)
-      m_angle.z = glm::wrapAngle(m_angle.z + velocidadeAngular.z * myTime);
-  }
-  //fmt::print("angle: {} {} {}\n", m_angle.x, m_angle.y, m_angle.z);
-
-  
-  // Clear color buffer and depth buffer
-  abcg::glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  //código praticamente padrão daqui em diante
-  abcg::glViewport(0, 0, m_viewportWidth, m_viewportHeight);
-  
-  abcg::glUseProgram(m_program); //usar shaders
-  abcg::glBindVertexArray(m_VAO); //usar vao
-
-  // atualizar variavel do angulo de rotação e posição de translação dentro do vertex shader
-  const GLint rotationXLoc{abcg::glGetUniformLocation(m_program, "rotationX")};
-  abcg::glUniform1f(rotationXLoc, m_angle.x);
-  const GLint rotationYLoc{abcg::glGetUniformLocation(m_program, "rotationY")};
-  abcg::glUniform1f(rotationYLoc, m_angle.y);
-  const GLint rotationZLoc{abcg::glGetUniformLocation(m_program, "rotationZ")};
-  abcg::glUniform1f(rotationZLoc, m_angle.z);
-  const GLint translationLoc{abcg::glGetUniformLocation(m_program, "translation")};
-  abcg::glUniform3fv(translationLoc, 1, &translation.x);
-
-  // Draw triangles
-  abcg::glDrawElements(GL_TRIANGLES, m_verticesToDraw, GL_UNSIGNED_INT,
-                       nullptr);
-
-  abcg::glBindVertexArray(0);
-  abcg::glUseProgram(0);
+    m_dices.paintGL(m_viewportWidth, m_viewportHeight, getDeltaTime());
 }
 
 void OpenGLWindow::paintUI() {
@@ -284,10 +152,9 @@ void OpenGLWindow::paintUI() {
     ImGui::PushItemWidth(200);
 
     if(ImGui::Button("Jogar!")){
-      tempoGirandoAleatorio();
-      velocidadeAngularAleatoria();
-      velocidadeDirecionalAleatoria();
-      dadoGirando = true;
+      for(auto &dice : m_dices.m_dices){
+        m_dices.jogarDado(dice);
+      }
       
     }
 
@@ -299,76 +166,6 @@ void OpenGLWindow::paintUI() {
   abcg::glFrontFace(GL_CW);
 }
 
-//função para começar o dado numa posição e número aleatório, além de inicializar algumas outras variáveis necessárias
-void OpenGLWindow::inicializarDado() {
-  // Inicializar gerador de números pseudo-aleatórios
-  auto seed{std::chrono::steady_clock::now().time_since_epoch().count()};
-  m_randomEngine.seed(seed);
-  
-  //estado inicial de algumas variáveis
-  m_rotation = {0, 0, 0};  
-  velocidadeAngular = {0.0f, 0.0f, 0.0f};
-  myTime = 0.0f;
-  quadros=0;
-
-  std::uniform_real_distribution<float> fdist(-1.5f,1.5f);
-  translation = {fdist(m_randomEngine),fdist(m_randomEngine),0.0f};
-  pousarDado(); //começar num numero aleatorio
-}
-
-//função para fazer o dado parar numa das faces retas aleatoriamente
-void OpenGLWindow::pousarDado() {
-  //reinicialização de variáveis para podermos parar o dado e jogar novamente
-  quadros = 0;
-  dadoGirando = false;
-  m_rotation = {0,0,0};
-
-  //fmt::print("translation final: {} {}\n", translation.x, translation.y);
-
-  std::uniform_int_distribution<int> idist(1,6);
-  const int numeroDoDado = idist(m_randomEngine);
-  m_angle.x = glm::radians(angulosRetos[numeroDoDado].x);
-  m_angle.y = glm::radians(angulosRetos[numeroDoDado].y);
-}
-
-//função para definir tempo de giro do dado, algo entre 2 e 5 segundos 
-void OpenGLWindow::tempoGirandoAleatorio(){
-  const float FPS = ImGui::GetIO().Framerate;
-  //distribuição aleatória para definir tempo de giro do dado, algo entre 2 e 5 segundos 
-  std::uniform_int_distribution<int> idist((int)FPS * 2, (int)FPS * 5);
-  maxQuadros = idist(m_randomEngine); //número máximo de quadros/vezes que o dado irá girar
-  //fmt::print("maxQuadros: {}\n",maxQuadros);
-}
-
-//atualiza as velocidades de cada um dos eixos de forma aleatória
-void OpenGLWindow::velocidadeAngularAleatoria(){
-  //distribuição aleatória entre 0 e 2, para girar somente 1 eixo
-  m_rotation = {0, 0, 0};
-  std::uniform_int_distribution<int> idist(0,2);
-  m_rotation[idist(m_randomEngine)] = 1;
-  //fmt::print("m_rotation.x: {} m_rotation.y: {} m_rotation.z: {}\n",m_rotation.x,m_rotation.y,m_rotation.z);
-
-  const float FPS = ImGui::GetIO().Framerate; //(para calcular os segundos precisamos do número inteiro de FPS)
-  //fmt::print("FPS: {}\n",FPS);
-
-  //distribuição aleatória de velocidade angular, para girar em cada eixo numa velocidade
-  std::uniform_real_distribution<float> fdist(FPS * 4, FPS * 8);
-  velocidadeAngular = {glm::radians(fdist(m_randomEngine))
-                      ,glm::radians(fdist(m_randomEngine))
-                      ,glm::radians(fdist(m_randomEngine))};
-  //fmt::print("velocidadeAngular.x: {} velocidadeAngular.y: {} velocidadeAngular.z: {}\n",velocidadeAngular.x,velocidadeAngular.y,velocidadeAngular.z);
-}
-
-//recebe uma das dimensões da janela e retorna uma fração aleatória do seu tamanho
-void OpenGLWindow::velocidadeDirecionalAleatoria(){
-  const float deltaTime{static_cast<float>(getDeltaTime())};
-  //distribuição aleatória de velocidade, para andar em cada eixo numa velocidade
-  std::uniform_real_distribution<float> fdist(deltaTime / 200.0f, deltaTime / 100.0f);
-  velocidadeDirecional.x = fdist(m_randomEngine) * m_viewportWidth;
-  velocidadeDirecional.y = fdist(m_randomEngine) * m_viewportHeight;
-  //fmt::print("velocidadeDirecionalAleatoria: {}\n", direcao);
-}
-
 void OpenGLWindow::resizeGL(int width, int height) {
   m_viewportWidth = width;
   m_viewportHeight = height;
@@ -376,7 +173,5 @@ void OpenGLWindow::resizeGL(int width, int height) {
 
 void OpenGLWindow::terminateGL() {
   abcg::glDeleteProgram(m_program);
-  abcg::glDeleteBuffers(1, &m_EBO);
-  abcg::glDeleteBuffers(1, &m_VBO);
-  abcg::glDeleteVertexArrays(1, &m_VAO);
+  m_dices.terminateGL();
 }
--- examples/dice/openglwindow.hpp
+++ examples/dice/openglwindow.hpp
@@ -4,16 +4,7 @@
 #include <vector>
 #include <random>
 #include "abcg.hpp"
-
-//atributos que definem um vértice: posição 3D, cor e operador == pra verificar se um vértice é igual a outro
-struct Vertex {
-  glm::vec3 position;
-  glm::vec3 color;
-
-  bool operator==(const Vertex& other) const {
-    return position == other.position;
-  }
-};
+#include "dices.hpp"
 
 class OpenGLWindow : public abcg::OpenGLWindow {
  protected:
@@ -24,49 +15,18 @@ class OpenGLWindow : public abcg::OpenGLWindow {
   void terminateGL() override;
 
  private:
-  GLuint m_VAO{};
-  GLuint m_VBO{};
-  GLuint m_EBO{};
   GLuint m_program{};
+  std::vector<Vertex> m_vertices; //arranjo de vértices lido do arquivo OBJ que será enviado ao VBO
+  std::vector<GLuint> m_indices; //arranjo de indices lido do arquivo OBJ que será enviado ao EBO
+  int m_verticesToDraw{}; //quantidade de vértices do VBO que será processada pela função de renderização, glDrawElements
+
+  Dices m_dices;
 
   int m_viewportWidth{};
   int m_viewportHeight{};
 
-  glm::vec3 m_angle{}; // ângulo de rotação que será enviado à variável uniforme do vertex shader.
-  int m_verticesToDraw{}; //quantidade de vértices do VBO que será processada pela função de renderização, glDrawElements
-  glm::ivec3 m_rotation{}; // nos ajuda a decidir qual a direção da rotação
-  glm::vec3 velocidadeAngular{}; //indica quantos graus/rad o dado deve girar por unidade de tempo, em cada um dos eixos x,y,z
-  glm::vec2 velocidadeDirecional{};
-  glm::vec3 translation{}; //indica a posição transladada do dado
-  //lista de ângulos cuja face do dado fica virada para a tela. Poderia ser maior, mas o resultado final seria pouco diferente.
-  std::array<glm::vec3, 7> angulosRetos{
-    glm::vec3{0.0f,0.0f,0.0f}, //0 apenas pra manter o numero do dado igual ao numero do indice
-    glm::vec3{125.0f,120.0f,45.0f}, //1
-    glm::vec3{345.0f,170.0f,15.0f}, //2
-    glm::vec3{75.0f,190.0f,13.0f}, //3
-    glm::vec3{75.0f,20.0f,77.0f},//4
-    glm::vec3{347.0f,342.0f,75.0f}, //5 
-    glm::vec3{105.0f,300.0f,45.0f} //6
-  };
-  glm::bvec2 movimentoDado{true, true}; //false = irá pra esquerda/baixo, true = irá pra direita/cima
-
-  std::default_random_engine m_randomEngine; //gerador de números pseudo-aleatórios
-
-  std::vector<Vertex> m_vertices; //arranjo de vértices lido do arquivo OBJ que será enviado ao VBO
-  std::vector<GLuint> m_indices; //arranjo de indices lido do arquivo OBJ que será enviado ao EBO
-  
-  float myTime{}; //auxiliar para conseguirmos não girar o tempo todo
-  bool dadoGirando = false; //indica se o dado deve estar girando 
-  int quadros; //contador de quadros, auxilia no tempo que o dado fica girando
-  int maxQuadros;
-
-  void loadModelFromFile(std::string path);
+  void loadModelFromFile(std::string_view path);
   void standardize(); 
-  void inicializarDado(); 
-  void pousarDado(); 
-  void velocidadeAngularAleatoria(); 
-  void velocidadeDirecionalAleatoria(); 
-  void tempoGirandoAleatorio();
 };
 
 #endif
